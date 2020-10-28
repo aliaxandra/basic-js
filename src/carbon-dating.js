4a5b4c9ce607d2240ca4e25762c2bce4c0747aed
Alexandra <aliaxandra@gmail.com>
2020-10-28 12:34:05 +0300

    feat: carbon-datin & dream-team solutions

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,17 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sample) {
+  
+  if (typeof sample !== 'string' || +sample >= 15 || +sample <= 0) {
+    return false;
+  }
+
+  if (/^\d+$/.test(sample) === false) {
+    return false;
+  }
+  
+  let decay = 0.693 / HALF_LIFE_PERIOD;
+
+  result = Math.log(MODERN_ACTIVITY / +sample) / decay;
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+  
+  if (arr === []) {
+    return null;
+  }
+
+  if (!Array.isArray(arr)) {
+    return false;
+  }
+
+  let newArr = arr.map(function (str) {
+    if (typeof str === 'string') {
+      return str.match(/[^\s]/)[0].toUpperCase();
+    }
+  })
+
+  function isString(something) {
+    return typeof (something) === 'string';
+  }
+
+  let sorted = newArr.filter(isString).slice().sort().join('');
+
+  return sorted;
 };
